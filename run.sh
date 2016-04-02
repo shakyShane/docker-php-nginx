16084271b80c2a983c4e7222ab9bb910a0630339
Shane Osbourne <shane.osbourne8@gmail.com>
2016-04-02 21:31:42 +0100

    Adding php site

--- run.sh
+++ run.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
 
-docker run --name php-fpm -v /Users/shakyshane/docker/src:/var/www/html -d sh/php
-docker run --name fpm-nginx -p 80:80 -v $(pwd):/etc/nginx/conf.d/ -d --link php-fpm:php-fpm nginx
+docker run --name php-fpm \
+    -v /Users/shakyshane/docker/src:/var/www/html \
+    -d php:7.0.4-fpm
+
+docker run --name fpm-nginx \
+    -p  8000:80 \
+    -v $(pwd)/nginx:/etc/nginx/conf.d/ \
+    -v /Users/shakyshane/docker/src:/var/www/html \
+    --link php-fpm:php-fpm \
+    -d nginx
+
+echo "--> requesting index.php"
+curl "$(docker-machine ip):8000/index.php"
+echo ""
+echo "--> requesting /"
+curl "$(docker-machine ip):8000"
+echo ""
+echo "URL: $(docker-machine ip):8000"
